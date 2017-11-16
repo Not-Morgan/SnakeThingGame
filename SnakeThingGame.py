dbfdc4eb9874c2853ab843f3a39c68cf49cbf908
Not-Morgan <masonanderson0@gmail.com>
2017-11-15 18:24:59 -0800

    added init and function outline for snake head class

--- SnakeThingGame.py
+++ SnakeThingGame.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 white = (255, 255, 255)
 black = (0,   0,     0)
@@ -12,6 +13,10 @@ display_width  = 1000
 #change to speed up of slow down game
 fps = 45
 
+top_wall = 0
+bottom_wall = 1000
+left_wall = 0
+right_wall = 1000
 
 pygame.init()
 gameDisplay = pygame.display.set_mode((display_width, display_height))
@@ -20,6 +25,23 @@ clock = pygame.time.Clock()
 crashed = False
 
 class snake:
+	pos = [0, 0]
+	direction = 0
+	size = 0
+	length = 0
+	colour = (0, 0, 0)
+
+	def __init__(self, size, color):
+		self.size = size
+		self.colour = color
+		self.pos[0] = random.randint(left_wall, right_wall)
+		self.pos[1] = random.randint(top_wall, bottom_wall)
+
+	def move(self, distance):
+		#some fancy trigonomentry goes here
+
+		def food_check():
+			#checks for food if it's there do stuff
 
 
 class snake_tail:
