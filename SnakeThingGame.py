1724ae55de1132ae80319e7c59ecf9792c7041c7
Not-Morgan <masonanderson0@gmail.com>
2017-11-18 09:35:59 -0800

    various fixes and clean up to make it run

--- SnakeThingGame.py
+++ SnakeThingGame.py
@@ -1,7 +1,7 @@
 import pygame
 import random 
 import math
-import store
+#import store
 
 white = (255, 255, 255)
 black = (0,   0,     0)
@@ -26,27 +26,25 @@ pygame.display.set_caption('Snake Thing Game')
 clock = pygame.time.Clock()
 crashed = False
 
-snake_heads = [snake_head(5)]
-foods = []
 
 class snake_head:
-	pos = [0, 0]
 	direction = 0
-	size = 0
 	length = 1
-	colour = (0, 0, 0)
 	tails = []
 
 	def __init__(self, size, color = green):
 		self.size = size
 		self.colour = color
+		self.pos = [0, 0]
 		self.pos[0] = random.randint(left_wall, right_wall)
 		self.pos[1] = random.randint(top_wall, bottom_wall)
 
 	def move(self, distance):
+		#move  a distance of distance
 		self.pos[0] += int(distance * math.sin(math.radians(self.direction)))
 		self.pos[1] += int(distance * math.cos(math.radians(self.direction)))
 		
+		#turn randomly
 		self.direction += random.randint(-15, 15)
 		self.direction % 360
 		
@@ -55,9 +53,11 @@ class snake_head:
 		if self.pos[0] < left_wall or self.pos[0] > right_wall:
 			self.direction += 180
 			
-		tails.append(snake_tail(self.pos, self.colour, self.size, length)
+		#create a new tail segment
+		self.tails.append(snake_tail(self.pos, self.length))
 		
 		self.food_check()
+
 
 	def food_check(self):
 		if 1 == 2:
@@ -65,38 +65,34 @@ class snake_head:
 			#what is this
 	
 	def tail_check(self):
-		for tail in tails:
-			if tail.check_if_dead() = True:
-				tails.remove(tail)
+		for tail in self.tails:
+			if self.tail.check_if_dead() == True:
+				self.tails.remove(tail)
 				
 
 
 class snake_tail:
-	pos = [0, 0]
-	colour = (0, 0, 0)
-	size = 0
-	last_time = 0
-	created_time = pygame.get_ticks()
-	
-	def __init__(self, snake_head_pos, colour, size, last_time):
+	def __init__(self, snake_head_pos, last_time):
+		created_time = pygame.time.get_ticks()
 		self.pos = snake_head_pos
-		self.colour = colour
-		self.size = size
 		self.last_time = last_time
 	
-	def check_if_dead(self)
-		if pygame.get_ticks() > created_time + last_time:
+	def check_if_dead(self):
+		if pygame.time.get_ticks() > self.created_time + self.last_time:
 			return True
 		
 
 class food:
-	pos = [0, 0]
-	size = 6
-	colour = red
-	def __init__(self):
+	def __init__(self, size, colour = red):
+		self.pos = [0, 0]
 		self.pos[0] = random.randint(left_wall, right_wall)
 		self.pos[1] = random.randint(top_wall, bottom_wall)
-	
+		self.colour = colour
+		self.size = size
+
+'''
+what is this for it's not even in the class
+
 def draw_food(self, color = red):
 	self.pos[0] = random.randint(left_wall, right_wall)
 	self.pos[1] = random.randint(top_wall, bottom_wall)
@@ -106,7 +102,7 @@ def draw_food(self, color = red):
 	
 	
 	print food
-	
+'''
 	
 
 
@@ -116,10 +112,8 @@ def Draw():
 	
 	#pygame.draw.line(gameDisplay, black, (top_wall
 	
-	for snake in snake_heads:
+	for snake in snakes:
 			pygame.draw.circle(gameDisplay, green, snake.pos, snake.size, 0)
-	
-	#draw things here
 	
 	for food in foods:
 		pygame.draw.circle(gameDisplay, food.colour, food.pos, 5, 0)
@@ -129,7 +123,8 @@ def Draw():
 
 def Logic():
 	#move snakes, eat food etc
-	snake.move(10)
+	for snake in snakes:
+		snake.move(10)
 	
 
 def Take_Input():
@@ -160,6 +155,9 @@ def prompt_purchase(score, price)
 		
 		
 """
+
+snakes = [snake_head(5)]
+foods = [food(5)]
 
 while not crashed:
 	Draw()
