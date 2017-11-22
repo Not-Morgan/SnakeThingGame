17fced082db1e19f3e1915b569df86a324279825
Josh <josh@joshs-mbp.vsb.bc.ca>
2017-11-22 11:36:19 -0800

    operational buttons, most bugs fixed

--- store.py
+++ store.py
@@ -21,7 +21,7 @@ def side_bar(screen):
         black = (0,0,0)
         
         
-
+        #define all the button positions
         
         button1_pos = [515, 100, 75, 50]
         button2_pos = [515, 200, 75, 50]
@@ -30,6 +30,7 @@ def side_bar(screen):
         
         font = pygame.font.SysFont(None, 17)
         
+        #define all the text displayed in the buttons        
         text1 = font.render("Apple", True, black)
         price1 = font.render("10 coins", True, black)
  
@@ -50,14 +51,16 @@ def side_bar(screen):
         def display_buttons(button_pos, text, price, colour, dim_colour):
                 if button_pos[0] + button_pos[2] > mouse[0] > button_pos[0] and button_pos[1] + button_pos[3] > mouse[1] > button_pos[1]: #if mouse is inside the button
                         pygame.draw.rect(screen, colour,(button_pos[0],button_pos[1],button_pos[2],button_pos[3])) #change colour
-                        screen.blit(text,(button_pos[0], button_pos[1])) #display text
-                        screen.blit(price,(button_pos[0], button_pos[1] + 10))
-                        
+                        screen.blit(text,(button_pos[0], button_pos[1])) #display product
+                        screen.blit(price,(button_pos[0], button_pos[1] + 10)) #display price
+                         
                        
                 else:
-                        pygame.draw.rect(screen, dim_colour,(button_pos[0],button_pos[1],button_pos[2],button_pos[3])) 
-                        screen.blit(text,(button_pos[0], button_pos[1]))
-                        screen.blit(price,(button_pos[0], button_pos[1] + 10))
+                        pygame.draw.rect(screen, dim_colour,(button_pos[0],button_pos[1],button_pos[2],button_pos[3])) #otherwise use the regular colour
+                        screen.blit(text,(button_pos[0], button_pos[1])) #display product
+                        screen.blit(price,(button_pos[0], button_pos[1] + 10)) #display price
+
+        #display all the buttons
 
         display_buttons(button1_pos, text1, price1, red,  dim_red)
         display_buttons(button2_pos, text2, price2, green, dim_green)
@@ -71,7 +74,6 @@ def button_pressed(score, price1, price2, price3, price4):
 
         snake_price = 10
         apple_price = 10
-        score_multiplier = 1.5
 
         button1_pos = [515, 100, 75, 50]
         button2_pos = [515, 200, 75, 50]
@@ -81,15 +83,15 @@ def button_pressed(score, price1, price2, price3, price4):
         mouse = pygame.mouse.get_pos()
 
         def buttonClicked(button_pos):
-                if button_pos[0] + button_pos[2] > mouse[0] > button_pos[0] and button_pos[1] + button_pos[3] > mouse[1] > button_pos[1]:
+                if button_pos[0] + button_pos[2] > mouse[0] > button_pos[0] and button_pos[1] + button_pos[3] > mouse[1] > button_pos[1]: #if the mouse is in the boundaries of the button
                         for event in pygame.event.get():
-                                if event.type == pygame.MOUSEBUTTONDOWN:
+                                if event.type == pygame.MOUSEBUTTONDOWN: #and the mouse is clicked
                                         return True
 
         if buttonClicked(button1_pos) and score >= price1:
                 return "Apple"
         if buttonClicked(button2_pos) and score >= price2:
-                return "Snake1"
+                return "Snake"
         if buttonClicked(button3_pos) and score >= price3:
                 return "Snake2"
         if buttonClicked(button4_pos) and score >= price4:
